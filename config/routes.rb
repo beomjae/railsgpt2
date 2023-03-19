d149d747ab240e4da18d02911871e4358ae411d5
leebeomjae <beomjae@gmail.com>
2023-03-19 12:28:17 +0000

    회원가입 기능 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   root "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
